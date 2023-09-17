c63ebbad00f3c5fb1b0c41b6414caa4a780fe679
kskxdfd <keisuke.nakasuga+githubXD@gmail.com>
2023-09-18 00:01:03 +0900

    Udemy セッション4 DB接続設定追加

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Item } from './item.model';
 import { ItemStatus } from './item-status.enum';
 import { CreateItemDto } from './dto/create-item.dto';
