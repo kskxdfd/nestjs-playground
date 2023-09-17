a94142d9f0fd4f05c830021839ddd4cc6f4a983c
kxdxf <keisuke.nakasuga+githubXD@gmail.com>
2023-09-17 16:58:08 +0900

    Udemy セッション3 Pipe、クラスバリデーターを追加

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,6 +1,18 @@
+import { IsInt, IsNotEmpty, IsString, MaxLength, Min } from 'class-validator';
+import { Type } from 'class-transformer';
+
 export class CreateItemDto {
-  id: string;
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(40)
   name: string;
+
+  @IsInt()
+  @Min(1)
+  @Type(() => Number)
   price: number;
+
+  @IsString()
+  @IsNotEmpty()
   description: string;
 }
