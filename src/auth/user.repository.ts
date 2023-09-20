78710d7110100c38215d7f0a5141bee545311133
kskxdfd <keisuke.nakasuga+githubXD@gmail.com>
2023-09-21 00:22:51 +0900

    Udemy セッション5 48 パスワードのハッシュ化を実装

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserRepository {
@@ -9,10 +10,13 @@ export class UserRepository {
 
   async createUser(createUserDto: CreateUserDto): Promise<User> {
     const { username, password, status } = createUserDto;
+    const salt = await bcrypt.genSalt();
+    const hashPassword = await bcrypt.hash(password, salt);
+
     return this.prisma.user.create({
       data: {
         username: username,
-        password: password,
+        password: hashPassword,
         status: status,
       },
     });
